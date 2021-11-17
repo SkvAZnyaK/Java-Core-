e395fbf4a8ea5e6ae23459d0245b055a1cba439e
SkvAZnyaK <zaraz4@yandex.ru>
2021-11-17 23:57:27 +0300

    HW9 added

--- src/main/java/lesson9/Main.java
+++ src/main/java/lesson9/Main.java
@@ -1,54 +1,60 @@
 package lesson9;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
-        List<Person> personList = new ArrayList<>();
-        personList.add(new Person("Oleg", 21, Person.Position.ENGINEER));
-        personList.add(new Person("Anton", 54, Person.Position.MANAGER));
-        personList.add(new Person("Oksana", 23, Person.Position.DIRECTOR));
-        personList.add(new Person("Aleksey", 43, Person.Position.ENGINEER));
-        personList.add(new Person("Andrey", 23, Person.Position.MANAGER));
-        personList.add(new Person("Oleg", 32, Person.Position.ENGINEER));
 
-        //Выбрать инженеров и и отсортировать их по возрасту
+     Student student1 = new Student("Anton", Arrays.asList(new Course("ASTROLOGY"), new Course("GO")));
+     Student student2 = new Student("Ivan", Arrays.asList(new Course("BIOLOGY"), new Course("GO")));
+     Student student3 = new Student("Michail", Arrays.asList(new Course("CHEMISTRY"), new Course("GO")));
+     Student student4 = new Student("Olga", Arrays.asList(new Course("PHP"), new Course("JAVA")));
+     Student student5 = new Student("Julia", Arrays.asList(new Course("PHP"), new Course("GO"), new Course("HTML")));
 
-        System.out.println(personList.stream()
-                .filter(p -> p.getPosition() == Person.Position.ENGINEER)
-                .sorted((p1, p2) -> p1.getAge() - p2.getAge())
+     ArrayList <Student> studentsList = new ArrayList<Student>();
+        studentsList.add(student1);
+        studentsList.add(student2);
+        studentsList.add(student3);
+        studentsList.add(student4);
+        studentsList.add(student5);
+
+     //uniqueCourses(studentsList);
+     //theMostInquisitiveStudent(studentsList);
+     //attendTheCourse(studentsList, new Course("GO"));
+
+        System.out.println("Список курсов, которые посещают студенты (без повторений): " + studentsList.stream()
+                .map(s -> s.getCourses())
+                .flatMap(c -> c.stream())
+                .map(c -> c.getCourseName())
+                .collect(Collectors.toSet()));
+
+        System.out.println("Самый любознательный студент: " + studentsList.stream()
+                .sorted((s1, s2) -> s2.getCourses().size() - s1.getCourses().size())
+                .limit(1)
                 .collect(Collectors.toList()));
 
-        Optional<Integer> sumAges = personList.stream()
-                .map(p -> p.getAge())
-                .reduce((p1, p2) -> p1 + p2);
+        Course currentCourse = new Course("GO");
+        System.out.println("Курс " + currentCourse + " посещают следующие студенты: " + studentsList.stream()
+                .filter(s -> s.getCourses().contains(currentCourse))
+                .collect(Collectors.toList()));
 
-        if (sumAges.isPresent()) {
-            System.out.println("Все прошло ок!");
-        }
-
-        Stream<Integer> integerStream = Stream.of();
-        Optional<Integer> optionalInteger = integerStream.reduce((a1, a2) -> a1 + a2);
-
-        if (optionalInteger.isPresent()) {
-            System.out.println("Результат: " + optionalInteger.get());
-        }
-
-        Stream.of("123", "1234", "123", "345345")
-                .distinct()
-                .forEach(p -> System.out.println(p));
-
-        Stream.of("123", "1234", "123", "345345")
-                .collect(Collectors.toSet());
-
-        Integer[][] integers = new Integer[][] {{1, 2, 3}, {4, 3, 2}};
-
-        Stream.of(integers)
-                .flatMap(a -> Stream.of(a))
-                .forEach(System.out::println);
     }
+
+//    private static void uniqueCourses(ArrayList studentsList) {
+//        //studentsList.stream()
+//                //.map(s -> s.getCourses())
+//                //.flatMap()
+//    }
+//
+//    private static void theMostInquisitiveStudent(ArrayList studentsList) {
+//
+//    }
+//
+//    private static void attendTheCourse(ArrayList studentsList, Course course) {
+//
+//    }
+
 }
